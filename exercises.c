52ea2d958ba3e8bd0aeebb7d613718a2939e0f53
Your Name <you@example.com>
2024-03-14 14:42:25 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -9,14 +9,20 @@ Ejercicio 1: Encuentra el Elemento Mayor
 Descripción: Escribe una función que tome un arreglo de enteros
 y su tamaño, y devuelva el valor más grande del arreglo.
 */
-int findMax(int arr[], int size) { return 0; }
+int findMax(int arr[], int size) { 
+
+  int temp = 1;
+  
+  return temp; }
 
 /*
 Ejercicio 2: Invertir un Arreglo
 Descripción: Escribe una función que tome un arreglo y su tamaño, y luego
 invierta el orden de sus elementos.
 */
-void reverseArray(int arr[], int size) {}
+
+
+//void reverseArray(int arr[], int size) {}
 
 /*
 Ejercicio 3: Filtrar Números Pares
@@ -26,7 +32,7 @@ los números pares del arreglo original.
 newsize apunta a una dirección válida que no ha sido inicializada con nigún valor específico. 
 *newsize debe almacenar el tamaño del nuevo arreglo que se retorna.
 */
-int *filterEvenNumbers(int arr[], int size, int *newSize) { return NULL; }
+//int *filterEvenNumbers(int arr[], int size, int *newSize) { return NULL; }
 
 /*
 Ejercicio 4: Fusión de dos Arreglos Ordenados
@@ -34,8 +40,8 @@ Descripción: Escribe una función que tome dos arreglos
 ordenados de menor a mayor y sus tamaños, y luego fusione estos dos
 arreglos en un tercer arreglo también ordenado de menor a mayor.
 */
-void mergeSortedArrays(int arr1[], int size1, int arr2[], int size2,
-                       int result[]) {}
+//void mergeSortedArrays(int arr1[], int size1, int arr2[], int size2,
+//int result[]) {}
 
 /*
 Ejercicio 5: Comprobación de Ordenación
@@ -43,7 +49,7 @@ Descripción: Escribe una función que tome un arreglo y su tamaño,
 y luego devuelva 1 si el arreglo está ordenado en orden ascendente,
   0 si no está ordenado, y -1 si está ordenado en orden descendente.
 */
-int checkSorted(int arr[], int size) { return -2; }
+//int checkSorted(int arr[], int size) { return -2; }
 
 /*
 Ejercicio 6: Información de una Biblioteca
@@ -53,6 +59,7 @@ información utilizando estructuras anidadas en C. Escribe la función para
 inicializar la información de un libro.
 */
 
+/*
 typedef struct {
   char nombre[50];
   int anioNacimiento;
@@ -64,9 +71,10 @@ typedef struct {
   int anioPublicacion;
 } Libro;
 
+
 void inicializarLibro(Libro *libro, const char *titulo, const char *nombreAutor,
                       int anioNacimiento, int anioPublicacion) {}
-
+*/
 /*
 Ejercicio 7: Lista enlazada de números
 Descripción: Escribe una función que tome un arreglo de enteros y su tamaño, y
@@ -79,9 +87,12 @@ Recuerda reservar memoria dinámica para cada nodo usando malloc.
 Puedes guiarte con lo que vimos en las clases (diapos).
   */
 
+/*
 typedef struct nodo {
   int numero;
   struct nodo *siguiente; // puntero al siguiente nodo
 } Nodo;
 
 Nodo *crearListaEnlazada(int arr[], int size) { return NULL; }
+*/
+
