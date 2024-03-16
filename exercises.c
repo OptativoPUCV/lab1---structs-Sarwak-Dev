e3546638289f741b83e138fd5878cfa33483bf48
Your Name <you@example.com>
2024-03-16 21:51:00 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -167,7 +167,7 @@ typedef struct nodo {
 Nodo *crearListaEnlazada(int arr[], int size) { 
   Nodo *head = NULL;
 
-  for (int i = 0; i < size; i++) {
+  for (int i = 0; i <= size; i++) {
     Nodo *nuevoNodo = (Nodo *)malloc(sizeof(Nodo));
     nuevoNodo->numero = arr[i];
     nuevoNodo->siguiente = head;
